6e96f3944a3467f9e7f2910ab6e75d4afc32b0a4
pedro prado <pedro.prado@precato.com.br>
2022-05-26 12:32:39 -0300

    feat: CONTINUACAO

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,6 +12,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (request, response) => {
+    console.log("NAU BIXO");
     return createCategoryController.handle(request, response);
 });
 
